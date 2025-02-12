6954d37a3ac8a9d14c13eb155f41818aced0518c
Nuraria <nurarihuonnomago1@email.com>
2025-02-12 18:19:11 +0200

    first commit

---
+++ spotify-backend/src/middleware/multer.js
@@ -0,0 +1,11 @@
+import multer from "multer";
+
+const storege = multer.diskStorage({
+  filename: function (req, file, callback) {
+    callback(null, file.originalname)
+  },
+});
+
+const upload = multer({storage});
+
+export default upload
